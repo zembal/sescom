9f6f86166f60602683592d8e242ad9274aae692d
Jarek <jarekzem@wp.pl>
2022-09-01 00:37:37 +0200

    part

--- src/app/services/network-status.service.ts
+++ src/app/services/network-status.service.ts
@@ -1,13 +1,19 @@
-import {Injectable} from '@angular/core';
-import {BehaviorSubject, fromEvent, map, merge, Observable, Subject} from "rxjs";
+import {EventEmitter, Injectable, OnDestroy} from '@angular/core';
+import {fromEvent, merge, Observable, Subject, takeUntil} from "rxjs";
 
 @Injectable({providedIn: 'root'})
-export class NetworkStatusService {
+export class NetworkStatusService implements OnDestroy {
 
+  private unsubscribe = new EventEmitter<boolean>();
   private networkStatus = new Subject<boolean>();
 
   constructor() {
     this.watchNetworkStatus();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next(true);
+    this.unsubscribe.complete();
   }
 
   public isOnline(): Observable<boolean> {
@@ -16,6 +22,7 @@ export class NetworkStatusService {
 
   private watchNetworkStatus(): void {
     merge(fromEvent(window, 'online'), fromEvent(window, 'offline'))
+      .pipe(takeUntil(this.unsubscribe))
       .subscribe(event => this.networkStatus.next(event.type === 'online'));
   }
 }
